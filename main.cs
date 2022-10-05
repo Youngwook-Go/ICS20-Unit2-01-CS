23fcf11456b7b05a2dd373ceb34434875cea753b
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-05 17:43:24 +0000

    a

--- main.cs
+++ main.cs
@@ -1,7 +1,13 @@
+// Created by: Youngwook
+// Created on: Oct 2022
+//
+// This program displays, "Hello, World!"
+
 using System;
 
 class Program {
     public static void Main (string[] args) {
         Console.WriteLine ("Hello World");
+        Console.WriteLine ("\nDone");
     }
 }
